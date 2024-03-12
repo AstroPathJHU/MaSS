175669022ea04ffe4a05721e5b8cb2153bfde68e
ssotodi1 <54324393+ssotodi1@users.noreply.github.com>
2024-03-11 21:02:20 -0400

    error log bug fix

--- shared/mywritetolog.m
+++ shared/mywritetolog.m
@@ -49,6 +49,7 @@ if locs == 1
     %
     str = ['MaSS-', tasktype, ' protocol started-v',version, '\n'];
     %
+    fprintf(str);
 end
 %
 % for error or warning messages write the message out in the correct format
@@ -58,6 +59,8 @@ if locs == 2
     if strcmpi(err_str, 'ERROR:')
         error(err_str);
     end
+    %
+    fprintf([err_str, '\n'])
     %
 end
 %
